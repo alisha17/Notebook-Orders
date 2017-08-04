6cdece12b6581e13a6070876f39a0f83cd652235
Alisha <anejaalisha@yahoo.com>
2017-08-04 21:00:37 +1000

    Final output left to be done

--- books.py
+++ books.py
@@ -1,36 +1,40 @@
-
 def a(lst, target, with_replacement=False):
-    def _a(idx, l, r, t, w):
-        if t == sum(l): r.append(l)
-        elif t < sum(l): return
+    
+    def _a(
+        idx,
+        l,
+        r,
+        t,
+        w,
+        ):
+
+        if t == sum(l):
+            r.append(l)
+        elif t < sum(l):
+            return
         for u in range(idx, len(lst)):
-            _a(u if w else (u + 1), l + [lst[u]], r, t, w)
+            _a((u if w else u + 1), l + [lst[u]], r, t, w)
         return r
+
     return _a(0, [], [], target, with_replacement)
 
 
 def get_best_combo(mylist, mysum):
-      # check sum is gettable first by calling once without repeating same item once
-  res = a(mylist, mysum)
-  if len(res) > 0:
-    return res
-  elif len(res) == 0:
-    # check sum by repeating same item in a boxes size
-    res1 = a(mylist, mysum, True)
-    if len(res1) > 0:
-      return res1
-    else:
-      # target is not gettable by any combination 
-      # the idea is recursively increase target by value 1 to get possible combination of elements
-      flag = True
-      res2 = []
-      while flag:
-        res2 = a(mylist, (mysum), True)
-        if len(res2) > 0:
-          flag=False
-          break;
+    res = a(mylist, mysum)
+    if len(res) > 0:
+        return res
+    elif len(res) == 0:
+        res1 = a(mylist, mysum, True)
+        if len(res1) > 0:
+            return res1
         else:
-          mysum+=1
-      return res2
-
-
+            flag = True
+            res2 = []
+            while flag:
+                res2 = a(mylist, mysum, True)
+                if len(res2) > 0:
+                    flag = False
+                    break
+                else:
+                    mysum += 1
+            return res2
--- main.py
+++ main.py
@@ -3,45 +3,81 @@ import csv
 from books import get_best_combo
 from tabulate import tabulate
 
+
 def main():
-    
-   book_list = []
-   books_in_box = []
+    book_list = []
+    books_in_box = []
+    order = []
 
-   with open("book_record.csv") as f:
-       records = csv.DictReader(f)
-       for row in records:
-            book_list.append(row) 
-       print book_list
+    with open('book_record.csv') as f:
+        records = csv.DictReader(f)
+        for row in records:
+            book_list.append(row)
 
-   headers = book_list[0].keys()
-   rows =  [x.values() for x in book_list]
-   print(tabulate(rows, headers=headers, tablefmt="grid"))
+    headers = book_list[0].keys()
+    rows = [x.values() for x in book_list]
 
-   subject = raw_input("Enter the subject.\n")
-   if not subject:
-       print ("Try again")
-   else:
-        cust_input = raw_input("Enter the number of books the customer wants.\n")
-        for x in book_list:
-            if x["subject"] == subject:
-                books_in_box.append(int(x["no_of_books"]))
+    print 'Welcome to Notebook Ordering system!\n'
+    print 'These are the number of books in box available subjectwise with their price.'
 
-   result = get_best_combo(books_in_box, int(cust_input))
+    print tabulate(rows, headers=headers)
 
-   final_sum = float('inf')
+    print "Let's proceed with the customer order now."
 
-   for i in result:
+    subject_no = \
+        raw_input('For how many subjects the customer wants to order?')
+
+    for i in range(0, int(subject_no)):
+        subject = raw_input('Enter the subject.\n')
+        if not subject:
+            print 'Try again'
+        else:
+            cust_input = \
+                raw_input('Enter the number of books the customer wants.\n'
+                          )
+            for x in book_list:
+                if x['subject'] == subject:
+                    books_in_box.append(int(x['no_of_books']))
+
+        a = get_final_price(books_in_box, int(cust_input), book_list,
+                            subject)
+        order.append(a)
+
+    for i in order:
+        if i["subject"] == subject:
+            for x in i["final_list"]:
+                i
+
+
+
+def get_final_price(
+    books_in_box,
+    cust_input,
+    book_list,
+    subject,
+    ):
+
+    result = get_best_combo(books_in_box, cust_input)
+
+    final_order = dict()
+    final_sum = float('inf')
+
+    for i in result:
         total_cost = 0
-        for x in i: 
+        for x in i:
             for item in book_list:
-                    if item['subject'] == subject and item['no_of_books'] == str(x):
-                        price_book = int(item["price"])
-                        total_cost = total_cost + price_book
+                if item['subject'] == subject and item['no_of_books'] \
+                    == str(x):
+                    price_book = int(item['price'])
+                    total_cost = total_cost + price_book
         if total_cost < final_sum:
             final_sum = total_cost
             final_list = i
-   print final_list,final_sum
 
-if __name__== "__main__":
+    final_order.update({'subject': subject, 'final_list': final_list,
+                       'final_sum': final_sum})
+    return final_order
+
+
+if __name__ == '__main__':
     main()
